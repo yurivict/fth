b75841870e1f73ee1a0ae3f3f0e8dde130f48b7a
mi-scholz <mi-scholz@356274cd-356a-4eb9-bd40-2da9e236b2b7>
2012-12-22 00:36:12 +0000

    bugfix

--- src/misc.c
+++ src/misc.c
@@ -26,7 +26,7 @@
  * Ident: $Id$
  */
 
-#define FTH_DATE                        "11-Dec-2012"
+#define FTH_DATE                        "22-Dec-2012"
 
 #if defined(HAVE_CONFIG_H)
 #include "config.h"
